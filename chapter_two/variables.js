9e470917e2056479a19ff89555ca17381cec4e06
ashraf-git <aliashraf.hena@gmail.com>
2020-12-09 12:53:01 +0600

    variables

--- chapter_two/variables.js
+++ chapter_two/variables.js
@@ -3,7 +3,7 @@ var age = 33
 console.log(name)
 console.log(age)
 
-console.log('HM Nayeem knows Javascript');
-console.log('His age is only 23')
-console.log('HM Nayeem is creating a course for all students')
-console.log('But his age is only 23')
+console.log(name + ' knows Javascript');
+console.log('His age is only '+ age)
+console.log( name + ' is creating a course for all students')
+console.log('But his age is only ' + age)
