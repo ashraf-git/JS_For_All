93449b7380fbd8de1e3fae2b2e1abea515f27338
ashraf-git <aliashraf.hena@gmail.com>
2020-12-09 20:58:10 +0600

    number datatypes

---
+++ chapter_two/datatypes.js
@@ -0,0 +1,23 @@
+// primitive
+
+// number
+// String
+// undefined
+// null
+
+// object
+
+// object
+// function
+// array
+
+// ****** number ****** number
+var n = 34;
+var f = 88.88;
+var nn = Number('53.67');
+console.log(nn);
+console.log(Number.parseInt(nn));
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(1/0);
+console.log('Number' * 20);
