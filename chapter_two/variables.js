7c83f72376e82b739a6f9e325b83ea417e1db98b
ashraf-git <aliashraf.hena@gmail.com>
2020-12-09 13:06:29 +0600

    reseved word

--- chapter_two/variables.js
+++ chapter_two/variables.js
@@ -7,3 +7,6 @@ console.log(name + ' knows Javascript');
 console.log('His age is only '+ age)
 console.log( name + ' is creating a course for all students')
 console.log('But his age is only ' + age)
+
+const else = 'ashraf'
+console.logK(else)
