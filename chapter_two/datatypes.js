84694fa5c6754de03cb82fd7830a490ffa90a4f1
ashraf-git <aliashraf.hena@gmail.com>
2020-12-09 21:06:32 +0600

    string datatypes

--- chapter_two/datatypes.js
+++ chapter_two/datatypes.js
@@ -11,13 +11,27 @@
 // function
 // array
 
-// ****** number ****** number
-var n = 34;
-var f = 88.88;
-var nn = Number('53.67');
-console.log(nn);
-console.log(Number.parseInt(nn));
-console.log(Number.MAX_VALUE);
-console.log(Number.MIN_SAFE_INTEGER);
-console.log(1/0);
-console.log('Number' * 20);
+// ****** number ****** 
+// var n = 34;
+// var f = 88.88;
+// var nn = Number('53.67');
+// console.log(nn);
+// console.log(Number.parseInt(nn));
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_SAFE_INTEGER);
+// console.log(1/0);
+// console.log('Number' * 20);
+
+// ****** String ****** 
+
+// literal string
+var str = '123';
+var str2 = "String";
+var str5 = `String`;
+
+// constructor string
+var str6 = String(3.54);
+var str3 = String("aliashraf");
+var str4 = String(25);
+
+console.log(str3, str5, str6)
