97a76af0cdf8da70a2b7057311f5dbed0e31b7a0
ashraf-git <aliashraf.hena@gmail.com>
2020-12-09 12:45:28 +0600

    hello

---
+++ chapter_two/hello.js
@@ -0,0 +1,6 @@
+console.log('Hello World!');
+console.log('Hello ashraf!');
+console.log(34)
+console.log(34.76)
+console.log(9+9)
+console.log('9'+9)
---
+++ chapter_two/variables.js
@@ -0,0 +1,9 @@
+var name = 'Mohammad Ali Ashraf Khan'
+var age = 33
+console.log(name)
+console.log(age)
+
+console.log('HM Nayeem knows Javascript');
+console.log('His age is only 23')
+console.log('HM Nayeem is creating a course for all students')
+console.log('But his age is only 23')
