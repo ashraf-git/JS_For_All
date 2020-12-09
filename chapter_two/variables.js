3c0b94b7c447c1b7ce4dcaeccb82ecff0b8b8379
ashraf-git <aliashraf.hena@gmail.com>
2020-12-09 13:17:36 +0600

    reseved word naming convention

--- chapter_two/variables.js
+++ chapter_two/variables.js
@@ -8,5 +8,9 @@ console.log('His age is only '+ age)
 console.log( name + ' is creating a course for all students')
 console.log('But his age is only ' + age)
 
-const else = 'ashraf'
-console.logK(else)
+// const else = 'ashraf'
+// console.logK(else)
+
+// var mathNumber = 34
+// var accountNumverDetailsId = 102
+// var account_number_details_id
