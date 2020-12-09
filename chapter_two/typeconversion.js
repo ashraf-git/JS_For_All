112b461364ffaea8bd7a36d754c3beb8aab22fde
ashraf-git <aliashraf.hena@gmail.com>
2020-12-09 21:41:14 +0600

    typeconversion

---
+++ chapter_two/typeconversion.js
@@ -0,0 +1,25 @@
+// var str = '1000'
+// var n = 10
+// console.log(Number.parseInt(str))
+// console.log(n.toString())
+// console.log(Number(Infinity))
+// console.log(Boolean(Infinity))
+// console.log(String(Infinity))
+// console.log(Boolean(-Infinity))
+
+//falsy values
+''
+0
+null
+undefined
+NaN
+
+console.log(Boolean(0))
+console.log(Boolean(''))
+console.log(Boolean('oiwheori'))
+console.log(Boolean(null))
+console.log(Boolean(undefined))
+console.log(Boolean(343))
+
+var x = true;
+console.log(x.toString())
