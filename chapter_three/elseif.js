2b2d9aada7705e95ded889fd3448f492fc39ae9b
ashraf-git <aliashraf.hena@gmail.com>
2020-12-10 14:27:08 +0600

    if else if condition

---
+++ chapter_three/elseif.js
@@ -0,0 +1,18 @@
+var a = 30;
+var b = 30;
+if(a > b){
+    console.log("A is greater than B");
+} else if (a < b) {
+    console.log("B is greater than A");
+} else {
+    console.log("They are same")
+}
+
+var n = 1
+if(n === 0) {
+    console.log(n +" is zero")
+} else if( n % 2 === 0 ){
+    console.log(n + ' is even number');
+} else {
+    console.log(n + ' is odd number');
+}
