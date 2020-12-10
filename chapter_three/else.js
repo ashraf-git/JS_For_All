d10a96742c66ef65c758913e4c1f35835b5404b8
ashraf-git <aliashraf.hena@gmail.com>
2020-12-10 14:15:22 +0600

    if else condition

---
+++ chapter_three/else.js
@@ -0,0 +1,15 @@
+var a = 30;
+var b = 40;
+if(a > b){
+    console.log("A is greater than B");
+} else {
+    console.log("B is greater than A");
+}
+
+var n = 51
+
+if( n%2 == 0 ){
+    console.log(n + ' is even number');
+} else {
+    console.log(n + ' is odd number');
+}
