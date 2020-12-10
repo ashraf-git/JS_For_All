1cecff3e2d0f9b05dfd812fe0d783c98eec09d72
ashraf-git <aliashraf.hena@gmail.com>
2020-12-10 08:25:52 +0600

    operators

---
+++ chapter_two/operator.js
@@ -0,0 +1,59 @@
+// Arithmetic operators
+// + - * / % ++ --
+// var a = 11
+// var b = 3
+// console.log(a % b);
+// Pre-increment - post increment
+// console.log(a++)
+// console.log(a)
+// console.log(b--)
+// console.log(b)
+
+//assignment operators
+// var a = 10;
+// var b = 20;
+//  a += b
+// console.log(a)
+//  a-=b
+//  console.log(a)
+//  a*=b
+//  console.log(a)
+//  a /= b
+//  console.log(a)
+//  a %= b
+//  console.log(a)
+
+//comparison operators
+// == != === !== > < >= <= 
+// var a = 20
+// var b = 10
+// console.log(a == b)// false
+// console.log(a != b)// true
+
+// console.log(a > b)// true
+// console.log(a < b)// false
+// b = 20
+// console.log(a >= b)// true
+// console.log(a <= b)// false/true
+
+// var c = '50'
+// var d = 50
+// console.log(c === d)//false
+// console.log(c !== d)//true
+
+// Logical operators
+// && 
+// || 
+// !
+
+//Bitwise operators
+// &
+// |
+// ~
+// ^
+// <<
+// >>
+
+// type of operator
+console.log(typeof "ashraf")
+console.log(typeof 52)
