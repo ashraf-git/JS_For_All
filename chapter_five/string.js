68db2d7292ec69a7b7eff8dc9c229df9c2ff4b47
ashraf-git <aliashraf.hena@gmail.com>
2020-12-11 20:46:37 +0600

    string comparison

--- chapter_five/string.js
+++ chapter_five/string.js
@@ -3,18 +3,30 @@
 // 'this is a string'
 // "this is a string"
 
-var str = 'something'//string literal
-var str2 = String('something')//string constructor
-console.log(str, str2)
+// var str = 'something'//string literal
+// var str2 = String('something')//string constructor
+// console.log(str, str2)
 
-var n = 10
-var str4 = n
-var str5 = n.toString()
-var str6 = n + ' '
-var str7 = String(n) //constructor
-console.log(n, str, str2, str4, str5, str6, str7)
+// var n = 10
+// var str4 = n
+// var str5 = n.toString()
+// var str6 = n + ' '
+// var str7 = String(n) //constructor
+// console.log(n, str, str2, str4, str5, str6, str7)
 //escape notation
-var str10 = " This \t is a 'string'\n"
-var str11 = 'This is \\a \'string\'\n'
-var str12 = 'This \\is\t a "string"'
-console.log(str10, str11, str12)
+// var str10 = " This \t is a 'string'\n"
+// var str11 = 'This is \\a \'string\'\n'
+// var str12 = 'This \\is\t a "string"'
+// console.log(str10, str11, str12)
+
+// carrige Return \r
+// vertical tab \v
+// backspace \b
+// Form feed \f
+
+// string comparison
+
+var x = 'abcS'
+var y = 'cdes'
+console.log(x<y)
+console.log('001' == 1)
