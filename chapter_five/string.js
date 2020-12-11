0f8dc80728a861323c5561b9bf512f34f11a280a
ashraf-git <aliashraf.hena@gmail.com>
2020-12-11 20:14:53 +0600

    constructor literal string

--- chapter_five/string.js
+++ chapter_five/string.js
@@ -1,4 +1,15 @@
 // what is string value
 
-'this is a string'
-"this is a string"
+// 'this is a string'
+// "this is a string"
+
+var str = 'something'//string literal
+var str2 = String('something')//string constructor
+console.log(str, str2)
+
+var n = 10
+var str4 = n
+var str5 = n.toString()
+var str6 = n + ' '
+var str7 = String(n) //constructor
+console.log(n, str, str2, str4, str5, str6, str7)
