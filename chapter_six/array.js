d0064605f332a2a2c047768ae5d8895e1a3b9a4e
ashraf-git <aliashraf.hena@gmail.com>
2020-12-13 18:02:14 +0600

    Declare Array

---
+++ chapter_six/array.js
@@ -0,0 +1,15 @@
+var arr = [1, 2, 3, 4, 5];
+console.log(arr)
+console.log(arr[3])
+console.log(arr[4])
+arr[5] = 6
+console.log(arr[5])
+console.log(arr)
+console.log(arr[7])
+arr[11] = 44
+console.log(arr)
+console.log(arr.length)
+arr[2] =22
+console.log(arr)
+arr2 = Array(10,14,54,54,878,65)
+console.log(arr2)
