ed3c42f5b31d8defc52172e31f5b815d3ebf763e
ashraf-git <aliashraf.hena@gmail.com>
2020-12-14 16:44:26 +0600

    object in array

---
+++ chapter_seven/object.js
@@ -0,0 +1,26 @@
+// Object in javascript
+// student 1:
+// Bangla : 34
+// English : 56
+// Math : 76
+
+// [34, 55, 56]
+
+// var obj = {}
+// obj.x = 10
+// console.log(obj)
+
+var point = {// Object literal
+    x: 10,
+    y: 20
+}
+// point.y = 30
+point.z = 30
+console.log(point)
+
+var obj = Object()//object constructor
+obj.a = 10
+var obj2 = new Object()//object constructor
+obj2.b = 39
+console.log(obj)
+console.log(obj2)
