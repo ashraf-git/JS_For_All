5e3055537a43091b9fd1a2ba486d0c711104a777
ashraf-git <aliashraf.hena@gmail.com>
2020-12-14 19:48:39 +0600

    access object properties

--- chapter_seven/object.js
+++ chapter_seven/object.js
@@ -10,17 +10,31 @@
 // obj.x = 10
 // console.log(obj)
 
-var point = {// Object literal
-    x: 10,
-    y: 20
-}
+// var point = {// Object literal
+//     x: 10,
+//     y: 20
+// }
 // point.y = 30
-point.z = 30
-console.log(point)
+// point.z = 30
+// console.log(point)
 
-var obj = Object()//object constructor
-obj.a = 10
-var obj2 = new Object()//object constructor
-obj2.b = 39
-console.log(obj)
-console.log(obj2)
+// var obj = Object()//object constructor
+// obj.a = 10
+// var obj2 = new Object()//object constructor
+// obj2.b = 39
+// console.log(obj)
+// console.log(obj2)
+
+var point = {// Object literal
+        x: 10,
+        y: 20,
+        z: 30
+}
+console.log(point.x)
+console.log(point.y)
+console.log(point.x + point.z)
+console.log(point['x'])//array notation
+console.log(point['z'])
+
+var show = "z"
+console.log(point[show])
