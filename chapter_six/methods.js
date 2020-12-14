b7f4768282a4f79d5787ffd6463c30bb191cfe95
ashraf-git <aliashraf.hena@gmail.com>
2020-12-14 15:48:58 +0600

    array method

---
+++ chapter_six/methods.js
@@ -0,0 +1,17 @@
+// var arr = [1, 2, 3, 4, 5, 6, 7, 10]
+// var arr2 = [47, 57, 57 , 57 , 85 ]
+// // console.log(arr.join(' | '))
+// // arr.fill(0)
+// var arr3 = arr.concat(arr2)
+// // console.log(arr)
+// console.log(arr3)
+// console.log(Array.isArray(arr))
+
+// var arr5 = Array.from(arr)
+// console.log(arr5)
+
+var a = [3, 4]
+var b = Array.from(a)
+b[1] = 9
+console.log(a)
+console.log(b)
