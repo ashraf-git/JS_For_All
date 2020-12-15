9b9638ee655357545279f5912b2502d8ebd43148
ashraf-git <aliashraf.hena@gmail.com>
2020-12-15 21:24:47 +0600

    define a function

---
+++ chapter_eight/function.js
@@ -0,0 +1,14 @@
+// var date = new Date()
+// date.getFullYear()
+
+function functionName(){
+
+}
+function add(){
+    var a = 20;
+    var b = 48;
+}
+function sub(){
+    var a = 40;
+    var b = 20;
+}
