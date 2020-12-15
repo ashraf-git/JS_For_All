075fd1e18f7f0aa275bc77c4c1173b03b21d21b2
ashraf-git <aliashraf.hena@gmail.com>
2020-12-15 21:41:05 +0600

    Declare function

--- chapter_eight/function.js
+++ chapter_eight/function.js
@@ -2,13 +2,24 @@
 // date.getFullYear()
 
 function functionName(){
-
+    console.log('I am a function')
 }
 function add(){
     var a = 20;
     var b = 48;
+    console.log(a+b)
 }
 function sub(){
     var a = 40;
     var b = 20;
+    console.log(a-b)
 }
+// functionName()
+// functionName()
+
+for(var i = 0; i < 10; i++){
+    functionName();
+}
+add();
+sub();
+console.log(add())
