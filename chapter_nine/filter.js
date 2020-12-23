8a31b6abb59ae17f911cfcdcd6b2d6fe554a7b64
ashraf-git <aliashraf.hena@gmail.com>
2020-12-23 07:54:43 +0600

    filter function

---
+++ chapter_nine/filter.js
@@ -0,0 +1,32 @@
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// var filterdArr = arr.filter(function(value) { 
+//     // return value %2 === 1
+//     return value > 4
+// })
+// console.log(filterdArr)
+
+// function myFilter(arr) {
+//     var newArr = []
+//     for (var i = 0; i < arr.length; i++){
+//         if(arr[i]%2 === 0){
+//             newArr.push(arr[i])
+//         }
+//     }
+//     return newArr
+// }
+// console.log(myFilter(arr))
+function myFilter(arr, cb) {
+    var newArr = []
+    for (var i = 0; i < arr.length; i++){
+        if(cb(arr[i], i, arr)){
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
+}
+console.log(myFilter(arr, function(value){
+    return value > 4
+}))
+console.log(myFilter(arr, function(value){
+    return value %2 === 1
+}))
