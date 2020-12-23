47d11987f16d176ea0aa15560de0d4d1c543ad6e
ashraf-git <aliashraf.hena@gmail.com>
2020-12-23 09:12:50 +0600

    reduce function

---
+++ chapter_nine/reduce.js
@@ -0,0 +1,31 @@
+var arr = [52, 1, 2, 585, 3, 4, 5]
+// var sum = arr.reduce(function(prev, curr){
+//     return prev + curr
+// }, 100)
+// console.log(sum)
+
+// var max = arr.reduce(function(prev, curr){
+//     return Math.max(prev, curr)
+// }, 0)
+// console.log(max)
+
+function myReduce(arr, cb, acc){
+    for(var i = 0; i < arr.length; i++) {
+        acc = cb(acc, arr[i])
+    }
+    return acc
+}
+var sum = myReduce(arr, function(prev, curr){
+    return prev + curr
+},0)
+console.log(sum)
+
+var max = myReduce(arr, function(prev, curr){
+    return Math.max(prev, curr)
+},0)
+console.log(max)
+
+var min = myReduce(arr, function(prev, curr){
+    return Math.min(prev, curr)
+}, arr[0])
+console.log(min)
